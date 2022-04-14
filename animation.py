5e97217656958762f017060584b31ae32b82cbe6
Till Hummel <tihummel@innosystec.de>
2022-04-14 10:04:00 +0200

    changed fx keys and added start animation

---
+++ animation.py
@@ -0,0 +1,24 @@
+import curses
+from time import sleep
+from common_resources import *
+
+
+def show_start_animation(std_scr):
+    height, width = std_scr.getmaxyx()
+
+    start_x_title = int((width // 2) - (TITLE_STR_LEN // 2))
+    start_y_title = int((height - TITLE_ARRAY_LEN) // 2)
+
+    start_x_sub = int((width // 2) - (len(SUBTITLE_PRESS_ANY) // 2))
+
+    for state in LOADING:
+        std_scr.clear()
+        for i in range(TITLE_ARRAY_LEN):
+            std_scr.addstr(start_y_title + i, start_x_title, state[i], curses.color_pair(1))
+        std_scr.refresh()
+        sleep(0.175)
+
+    std_scr.clear()
+    for i in range(TITLE_ARRAY_LEN):
+        std_scr.addstr(start_y_title + i, start_x_title, title[i], curses.color_pair(1))
+    std_scr.refresh()
--- tui.py
+++ tui.py
@@ -2,11 +2,23 @@ import curses
 import start_pad
 import test_dices
 import player_card
+import animation
 from common_resources import *
+import KeyUtils
+
+
+def show_current(key, std_scr):
+    if key == KeyUtils.VK_1:
+        player_card.show_player_cards(std_scr)
+    elif key == KeyUtils.VK_2:
+        test_dices.show_dices(std_scr)
+    elif key == KeyUtils.VK_BACKSPACE:
+        start_pad.show_pad(std_scr)
 
 
 def show_tui(std_scr):
     key = 0
+    old_key = KeyUtils.VK_BACKSPACE
     padding = 5
 
     std_scr.clear()
@@ -17,9 +29,16 @@ def show_tui(std_scr):
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_CYAN)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    while key != 27:
+    curses.resize_term(80, 180)
+
+    while key == 0:
         std_scr.clear()
-        curses.resize_term(45, 180)
+        animation.show_start_animation(std_scr)
+
+        key = std_scr.getch()
+
+    while key != KeyUtils.VK_ESCAPE:
+        std_scr.clear()
         height, width = std_scr.getmaxyx()
 
         start_x_title = int((width // 2) - (TITLE_STR_LEN // 2))
@@ -28,14 +47,17 @@ def show_tui(std_scr):
         for i in range(TITLE_ARRAY_LEN):
             std_scr.addstr(i, start_x_title, title[i], curses.color_pair(1))
 
-        if key == 265:
+        if key == KeyUtils.VK_1:
             player_card.show_player_cards(std_scr)
-        elif key == 266:
+            old_key = key
+        elif key == KeyUtils.VK_2:
             test_dices.show_dices(std_scr)
-        elif key == 8:
+            old_key = key
+        elif key == KeyUtils.VK_BACKSPACE:
             start_pad.show_pad(std_scr)
+            old_key = key
         else:
-            start_pad.show_pad(std_scr)
+            show_current(old_key, std_scr)
 
         std_scr.addstr(height-1, 0, STATUS_BAR_STR, curses.color_pair(2))
         std_scr.addstr(height-1, len(STATUS_BAR_STR), " " * (width - len(STATUS_BAR_STR) - 1), curses.color_pair(2))
