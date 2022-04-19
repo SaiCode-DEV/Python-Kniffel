d74e4ef389af2e0e42cb44195e79133fa0f69ac8
Till Hummel <tihummel@innosystec.de>
2022-04-19 14:55:43 +0200

    make animation as class

--- animation.py
+++ animation.py
@@ -1,24 +1,28 @@
 import curses
-from time import sleep
 from common_resources import *
 
 
-def show_start_animation(std_scr):
-    height, width = std_scr.getmaxyx()
+class StartAnimation:
 
-    start_x_title = int((width // 2) - (TITLE_STR_LEN // 2))
-    start_y_title = int((height - TITLE_ARRAY_LEN) // 2)
+    def __init__(self):
+        self.step_count = 0
 
-    for state in LOADING:
+    def show_next_step(self, std_scr):
+        height, width = std_scr.getmaxyx()
+
+        start_x_title = int((width // 2) - (TITLE_STR_LEN // 2))
+        start_y_title = int((height - TITLE_ARRAY_LEN) // 2)
+
         std_scr.clear()
-        for i in range(TITLE_ARRAY_LEN):
-            std_scr.addstr(start_y_title + i, start_x_title, state[i], curses.color_pair(1))
+        if self.step_count <= len(LOADING):
+            for i in range(TITLE_ARRAY_LEN):
+                std_scr.addstr(start_y_title + i, start_x_title, LOADING[self.step_count], curses.color_pair(1))
+        else:
+            for i in range(TITLE_ARRAY_LEN):
+                std_scr.addstr(start_y_title + i, start_x_title, title[i], curses.color_pair(1))
         std_scr.refresh()
-        sleep(0.175)
 
-    std_scr.clear()
-    for i in range(TITLE_ARRAY_LEN):
-        std_scr.addstr(start_y_title + i, start_x_title, title[i], curses.color_pair(1))
-    std_scr.refresh()
+        self.step_count += 1
 
-    sleep(1)
+        if self.step_count > len(LOADING):
+            self.step_count = 0
