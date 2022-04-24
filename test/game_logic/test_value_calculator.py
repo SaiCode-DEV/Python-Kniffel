c21513d36216e8c778dcd8cdbd5a3d3a8908c742
janschaible <jaschaib@innosystec.de>
2022-04-24 21:50:14 +0200

    added test for start animation

--- test/game_logic/test_value_calculator.py
+++ test/game_logic/test_value_calculator.py
@@ -5,6 +5,7 @@ Testing the valueCalculator Functions
 # pylint: disable=C
 
 from kniffel.game_logic.value_calculator import *
+from kniffel.data_objects.combinations import *
 
 from unittest import TestCase
 from typing import Callable, List
@@ -31,6 +32,10 @@ class TestValueCalculator(TestCase):
         self.assertEqual(3, result, "1,[1, 1, 1, 2, 6] should result to 3")
         result = get_number_value(5, [1, 3, 1, 2, 6])
         self.assertEqual(0, result, "5,[1, 3, 1, 2, 6] should result to 0")
+        for val in range(6):
+            calc_fn = get_calc_fn(Combinations(val))
+            res = calc_fn([val + 1 for _ in range(5)])
+            self.assertEqual((val + 1) * 5, res, f"failed to sum number up or got wrong calc fn")
 
     def test_get_three_of_kind(self):
         self.invalid_throw(lambda x: get_three_of_kind_value(x))
@@ -38,6 +43,7 @@ class TestValueCalculator(TestCase):
         self.assertEqual(11, result, "[1,1,1,2,6] should result to 11")
         result = get_three_of_kind_value([1, 3, 1, 2, 6])
         self.assertEqual(0, result, "[1,3,1,2,6] should result to ß")
+        self.assertEqual(get_three_of_kind_value,get_calc_fn(Combinations.THREE_OF_KIND))
 
     def test_get_four_of_kind(self):
         self.invalid_throw(lambda x: get_four_of_kind_value(x))
@@ -45,6 +51,7 @@ class TestValueCalculator(TestCase):
         self.assertEqual(0, result, "[1,1,1,2,6] should result to 0")
         result = get_four_of_kind_value([1, 1, 1, 1, 6])
         self.assertEqual(10, result, "[1,3,1,2,6] should result to 10")
+        self.assertEqual(get_four_of_kind_value,get_calc_fn(Combinations.FOUR_OF_KIND))
 
     def test_get_full_house(self):
         self.invalid_throw(lambda x: get_full_house_value(x))
@@ -52,6 +59,7 @@ class TestValueCalculator(TestCase):
         self.assertEqual(0, result, "[1,1,3,2,2] should result to 0")
         result = get_full_house_value([1, 1, 1, 2, 2])
         self.assertEqual(FULL_HOUSE_VALUE, result, f"[1,1,1,2,2] should result to {FULL_HOUSE_VALUE}")
+        self.assertEqual(get_full_house_value,get_calc_fn(Combinations.FULL_HOUSE))
 
     def test_count_run(self):
         result = count_run([1, 1, 3, 2, 2])
@@ -71,6 +79,7 @@ class TestValueCalculator(TestCase):
         self.assertEqual(SMALL_STRAIGHT_VALUE, result, f"[1, 4, 3, 2, 5] should result to {SMALL_STRAIGHT_VALUE}")
         result = get_small_straight_value([1, 3, 1, 2, 1])
         self.assertEqual(0, result, "[1, 3, 1, 2, 1] should result to 0")
+        self.assertEqual(get_small_straight_value,get_calc_fn(Combinations.SMALL_STRAIGHT))
 
     def test_large_straight(self):
         self.invalid_throw(lambda x: get_large_straight_value(x))
@@ -80,6 +89,7 @@ class TestValueCalculator(TestCase):
         self.assertEqual(LARGE_STRAIGHT_VALUE, result, f"[1, 4, 3, 2, 5] should result to {LARGE_STRAIGHT_VALUE}")
         result = get_large_straight_value([6, 5, 4, 3, 3])
         self.assertEqual(0, result, "[6, 5, 4, 3, 3] should result to 0")
+        self.assertEqual(get_large_straight_value,get_calc_fn(Combinations.LARGE_STRAIGHT))
 
     def test_kniffel(self):
         self.invalid_throw(lambda x: get_kniffel_value(x))
@@ -87,6 +97,7 @@ class TestValueCalculator(TestCase):
         self.assertEqual(0, result, f"[1,1,2,1,1] should result to 0")
         result = get_kniffel_value([4, 4, 4, 4, 4])
         self.assertEqual(KNIFFEL_VALUE, result, f"[4,4,4,4,4] should result to {KNIFFEL_VALUE}")
+        self.assertEqual(get_kniffel_value,get_calc_fn(Combinations.KNIFFEL))
 
     def test_chance(self):
         self.invalid_throw(lambda x: get_chance_value(x))
@@ -94,3 +105,4 @@ class TestValueCalculator(TestCase):
         self.assertEqual(6, result, "[1,1,2,1,1] should result to 6")
         result = get_chance_value([4, 4, 4, 4, 4])
         self.assertEqual(20, result, "[4,4,4,4,4] should result to 20")
+        self.assertEqual(get_chance_value,get_calc_fn(Combinations.CHANCE))
---
+++ test/windows/animations/test_stat_animation.py
@@ -0,0 +1,34 @@
+# pylint: disable=C
+
+from test.windows.window_test import WindowTest
+from kniffel.windows.animations.start_animation import *
+
+
+class StartAnimationTest(WindowTest):
+
+    def __init__(self, method_name='runTest'):
+        super().__init__(method_name)
+        self.start_animation: StartAnimation = None
+
+    def setUp(self):
+        super().setUp()
+        self.start_animation = StartAnimation(self.window)
+
+    def tearDown(self) -> None:
+        del self.window
+
+    def get_max_yx(self):
+        return StartAnimation.get_required_size()
+
+    def test_game_card_render(self):
+        for loading in common.LOADING:
+            self.start_animation.render()
+            actual = self.get_screen_value()
+
+            if len(actual) != len(loading):
+                raise AssertionError("Expected and actual differ in length")
+            iteration = 0
+            for line in loading:
+                self.assertEqual(line.rstrip(), actual[iteration].rstrip(), f"did either not advance animation or did not render correctly error in line {iteration + 1}")
+                iteration += 1
+            self.start_animation.step_animation()
