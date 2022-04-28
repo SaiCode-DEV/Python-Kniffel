53e14bec1c60fafbe188b9c8e6feac93b27976d9
janschaible <jaschaib@innosystec.de>
2022-04-28 15:43:33 +0200

    adjust bot for use of combination enum

--- kniffel/bot/bot.py
+++ kniffel/bot/bot.py
@@ -1,65 +1,51 @@
 # https://brefeld.homepage.t-online.de/kniffel.html
 import random
-from game_logic.value_calculator import *
 import sys
+from numpy import diff
+
+from kniffel.data_objects.combinations import Combinations
+from kniffel.game_logic.value_calculator import *
 import numpy as np
 from numpy import asarray as ar
 
-from numpy import diff
-sys.path.append('../')
-POSSIBLE = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "three_of_a_kind": 3,
-    "four_of_a_kind": 4,
-    "full_house": 25,
-    "small_straight": 30,
-    "large_straight": 40,
-    "yahtzee": 50,
-    "chance": 0}
 
-
-def get_best_choice(gewuerfelt, available) -> dict:
+def get_best_choice(dice: List[int], available_combinations: List[Combinations]) -> dict:
     generate = []
-    if(len(available) == 0):
+    if len(available_combinations) == 0:
         return {}
-    for i in range(len(available)):
+    for combination in available_combinations:
         # calculate the value of the throw
-        match available[i]:
-            case "one":
-                generate.append(get_one_value(gewuerfelt) / 1 * 5 + 1)
-            case "two":
-                generate.append(get_two_value(gewuerfelt) / 2 * 5 + 2)
-            case "three":
-                generate.append(get_three_value(gewuerfelt) / 3 * 5 + 3)
-            case "four":
-                generate.append(get_four_value(gewuerfelt) / 4 * 5 + 4)
-            case "five":
-                generate.append(get_five_value(gewuerfelt) / 5 * 5 + 5)
-            case "six":
-                generate.append(get_six_value(gewuerfelt) / 6 * 5 + 6)
-            case "three_of_a_kind":
-                generate.append(get_three_of_kind_value(gewuerfelt))
-            case "four_of_a_kind":
-                generate.append(get_four_of_kind_value(gewuerfelt))
-            case "full_house":
-                generate.append(get_full_house_value(gewuerfelt))
-            case "small_straight":
-                generate.append(get_small_straight_value(gewuerfelt))
-            case "large_straight":
-                generate.append(get_large_straight_value(gewuerfelt))
-            case "yahtzee":
-                generate.append(get_kniffel_value(gewuerfelt))
-            case "chance":
+        match combination.value:
+            case Combinations.ONES.value:
+                generate.append(get_one_value(dice) / 1 * 5 + 1)
+            case Combinations.TWOS.value:
+                generate.append(get_two_value(dice) / 2 * 5 + 2)
+            case Combinations.THREES.value:
+                generate.append(get_three_value(dice) / 3 * 5 + 3)
+            case Combinations.FOURS.value:
+                generate.append(get_four_value(dice) / 4 * 5 + 4)
+            case Combinations.FIVES.value:
+                generate.append(get_five_value(dice) / 5 * 5 + 5)
+            case Combinations.SIXES.value:
+                generate.append(get_six_value(dice) / 6 * 5 + 6)
+            case Combinations.THREE_OF_KIND.value:
+                generate.append(get_three_of_kind_value(dice))
+            case Combinations.FOUR_OF_KIND.value:
+                generate.append(get_four_of_kind_value(dice))
+            case Combinations.FULL_HOUSE.value:
+                generate.append(get_full_house_value(dice))
+            case Combinations.SMALL_STRAIGHT.value:
+                generate.append(get_small_straight_value(dice))
+            case Combinations.LARGE_STRAIGHT.value:
+                generate.append(get_large_straight_value(dice))
+            case Combinations.KNIFFEL.value:
+                generate.append(get_kniffel_value(dice))
+            case Combinations.CHANCE.value:
                 # to make it less interesting
-                generate.append(get_chance_value(gewuerfelt) - 10)
+                generate.append(get_chance_value(dice) - 10)
 
-    if(len(generate) == len(available)):
-        possible = dict(zip(available, generate))
+    if len(generate) == len(available_combinations):
+        possible = dict(zip(available_combinations, generate))
         # sort out all the impossible / useless combinations
         possible = {a: b for a, b in possible.items() if b != 0}
         # print(possible)
@@ -74,74 +60,72 @@ def calculate_expected_value(gewuerfelt, available):
     return 0
 
 
-def reroll_controller(gewuerfelt, available):
+def reroll_controller(dice_rolled, available_combinations):
     # try out each possible dice combination
     dices = []
     for dice in range(6):
-        dices.append(sorted([dice + 1] + gewuerfelt[1:], reverse=True))
+        dices.append(sorted([dice + 1] + dice_rolled[1:], reverse=True))
         dices.append(
-            sorted(gewuerfelt[:1] + [dice + 1] + gewuerfelt[2:], reverse=True))
+            sorted(dice_rolled[:1] + [dice + 1] + dice_rolled[2:], reverse=True))
         dices.append(
-            sorted(gewuerfelt[:2] + [dice + 1] + gewuerfelt[3:], reverse=True))
+            sorted(dice_rolled[:2] + [dice + 1] + dice_rolled[3:], reverse=True))
         dices.append(
-            sorted(gewuerfelt[:3] + [dice + 1] + gewuerfelt[4:], reverse=True))
-        dices.append(sorted(gewuerfelt[:4] + [dice + 1], reverse=True))
-    print(f"orginal: {gewuerfelt}\n")
+            sorted(dice_rolled[:3] + [dice + 1] + dice_rolled[4:], reverse=True))
+        dices.append(sorted(dice_rolled[:4] + [dice + 1], reverse=True))
+    print(f"orginal: {dice_rolled}\n")
     # print(dices)
     dices = list(set({tuple(i) for i in dices}))
     dices = sorted([list(i) for i in dices])
     output_points = []
     for dice in dices:
-        output_points.append(get_best_choice(dice, available))
+        output_points.append(get_best_choice(dice, available_combinations))
     output_points, dices = zip(*sorted(zip(output_points, dices),
-                               key=lambda x: x[0].get(next(iter(x[0])), 0), reverse=True))
+                                       key=lambda x: x[0].get(next(iter(x[0])), 0), reverse=True))
     max_points = max([i.get(next(iter(i)), 0) for i in output_points])
     different = []
     for option, dice in zip(output_points, dices):
-        if(option.get(next(iter(option)), 0) == max_points):
-            #print(f"{dice} | {next(iter(option))} is the best with {option.get(next(iter(option)))} points")
+        if option.get(next(iter(option)), 0) == max_points:
+            # print(f"{dice} | {next(iter(option))} is the best with {option.get(next(iter(option)))} points")
             different.append(dice)
     changed_dices = [False, False, False, False, False]
     for difference in different:
         changed_dices = ar(np.array(difference) != np.array(
-            gewuerfelt)) | ar(changed_dices)
+            dice)) | ar(changed_dices)
     return changed_dices
 
     # get the elements with the highest value
 
 
-def bot_controller(gewuerfelt: list[int], available, left_rerolls=0) -> str:
+def bot_controller(dice: list[int], available_combinations, rerolls_left=0) -> Combinations:
     # sort the dices by value
-    gewuerfelt = sorted(gewuerfelt, reverse=True)
-    print(gewuerfelt)
-    best_now = get_best_choice(gewuerfelt, available)
+    dice_sorted = sorted(dice, reverse=True)
+    print(dice_sorted)
+    best_now = get_best_choice(dice_sorted, available_combinations)
     print(best_now)
-    if (available == []):
-        return "nothing to choose"
-    if (best_now.get("yahtzee") == 50):
+    if best_now.get(Combinations.KNIFFEL) == 50:
         print("yahtzee is the best choice")
-        return "yahtzee"
-    elif(best_now.get("large_straight") == 40):
+        return Combinations.KNIFFEL
+    elif best_now.get(Combinations.LARGE_STRAIGHT) == 40:
         print("large straight is the best choice")
-        return "large_straight"
-    elif(best_now.get("small_straight") == 30):
+        return Combinations.LARGE_STRAIGHT
+    elif best_now.get(Combinations.SMALL_STRAIGHT) == 30:
         print("small straight is the best choice")
-        return "small_straight"
-    elif(best_now.get("full_house") == 25):
+        return Combinations.SMALL_STRAIGHT
+    elif best_now.get(Combinations.FULL_HOUSE) == 25:
         print("full house is the best choice")
-        return "full_house"
-    elif(best_now.get("four_of_a_kind") if best_now.get("four_of_a_kind") is not None else 0 > 5):
+        return Combinations.FULL_HOUSE
+    elif best_now.get(Combinations.FOUR_OF_KIND) if best_now.get(Combinations.FOUR_OF_KIND) is not None else 0 > 5:
         print("four of a kind is the best choice")
-        return "four_of_a_kind"
-    elif(best_now.get("three_of_a_kind") if best_now.get("three_of_a_kind") is not None else 0 > 6):
+        return Combinations.FOUR_OF_KIND
+    elif best_now.get(Combinations.THREE_OF_KIND) if best_now.get(Combinations.THREE_OF_KIND) is not None else 0 > 6:
         print("three of a kind is the best choice")
-        return "three_of_a_kind"
+        return Combinations.THREE_OF_KIND
     else:
         print("There is no good special combination")
-        if(left_rerolls > 0):
+        if rerolls_left > 0:
             print("reroll")
             # choose the cubes to reroll
-            return reroll_controller(gewuerfelt, available)
+            return reroll_controller(dice_sorted, available_combinations)
         else:
             print("no rerolls left, choose the best left over")
             best = next(iter(best_now))
@@ -151,28 +135,31 @@ def bot_controller(gewuerfelt: list[int], available, left_rerolls=0) -> str:
 
 
 if __name__ == "__main__":
-    #[1, 2, 5, 3, 5]
-    # ranom cubes
-    gewuerfelt = [random.randint(1, 6) for i in range(5)]
+    # [1, 2, 5, 3, 5]
+    # random cubes
+    gewuerfelt = [1, 2, 5, 3, 5]
     available = [
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "four_of_a_kind",
-        "full_house",
-        "small_straight",
-        "large_straight",
-        "yahtzee",
-        "chance"]
+        Combinations.ONES,
+        Combinations.TWOS,
+        Combinations.THREES,
+        Combinations.FOURS,
+        Combinations.FIVES,
+        Combinations.SIXES,
+        Combinations.THREE_OF_KIND,
+        Combinations.FOUR_OF_KIND,
+        Combinations.FULL_HOUSE,
+        Combinations.SMALL_STRAIGHT,
+        Combinations.LARGE_STRAIGHT,
+        Combinations.KNIFFEL,
+        Combinations.CHANCE
+
+    ]
     left_rerolls = 1
     choice = bot_controller(gewuerfelt, available, left_rerolls)
     print(choice)
 
 
-def test(available):
+def test(available_combinations):
     # try out each possible dice combination
     dices = []
     for dice1 in range(6):
@@ -187,6 +174,6 @@ def test(available):
     dices = sorted([list(i) for i in dices])
     print(len(dices))
     for dice in dices:
-        new_value = get_best_choice(dice, available)
+        new_value = get_best_choice(dice, available_combinations)
         # subdivide 10 from chance
         print(f"{dice} | {next(iter(new_value))} is the best with {new_value.get(next(iter(new_value)))} points")
